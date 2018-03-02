f30345d0dbe63c8ae378c71d866cf5becf6baafe
arodg <anitarodgerson@gmail.com>
2018-03-02 17:42:57 -0600

    Add sequelize method to routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,41 +1,42 @@
-// All code below based on Class Activity-CatsApp
+
 const express = require("express");
-const burgers = require("../models/burgers.js");
 const router = express.Router();
 
+const db = require("../models");
 
-// Create all our routes and set up logic within those routes where required.
+
+
 router.get("/", function(req, res) {
-  burgers.selectAll(function(data) {
+  db.burgers.findAll({}).then(function(data) {
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
 
 router.post("/api/burgers", function(req, res) {
-  burgers.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    res.json({id: result.insertID});
-  });
+  db.burgers.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured
+    }).then(function(result) {
+      res.json({id: result.insertID});
+    });
 });
+
 
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burgers.updateOne({
-    devoured: true
-  }, condition, function(result) {
+  db.burgers.update({
+    devoured: req.body.devoured
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function(result) {
     res.status(200).end();
-  });
+  }); 
 });
 
 
-// Export routes for server.js to use.
 module.exports = router;
