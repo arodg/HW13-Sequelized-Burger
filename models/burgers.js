971ab5b3e01c4817163f4627093e0eccc32d0527
arodg <anitarodgerson@gmail.com>
2018-03-02 13:54:07 -0600

    Change MySQL model to Sequelized model

--- models/burgers.js
+++ models/burgers.js
@@ -1,29 +1,18 @@
-// Code below based on Class Activity-CatsApp
-// Import the ORM to create functions that will interact with the database.
-const orm = require("../config/orm.js");
-const connection = require("../config/connection.js")
+module.exports = function(sequelize, DataTypes) {
+  var burgers = sequelize.define("burgers", {
+    
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-var burgers = {
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
-
-  insertOne: function(cols, vals ,cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  
+  });
+    
+  return burgers;
 };
 
-  
-
-// Export the database functions for the controller (burgers_controllers.js).
-module.exports = burgers;
