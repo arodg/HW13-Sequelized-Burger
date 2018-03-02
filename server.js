3b0c752dc3b4a0b3a6869f38f56a629aaf98587e
arodg <anitarodgerson@gmail.com>
2018-03-02 17:35:11 -0600

    Sync sequelize models

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,9 +18,12 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controllers.js");
+
 app.use("/", routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
 
+db.sequelize.sync({force: true}).then(function() {
+	app.listen(PORT,function() {
+		console.log("App now listening at localhost:" + PORT);
+	});
+});
